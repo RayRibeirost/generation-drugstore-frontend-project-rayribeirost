ed1cf1d2879d70931150e7fa2e6199f588530e7c
Raylander Ribeiro (rayribeirost) <ribeiroraylander7@gmail.com>
2026-02-25 15:22:33 -0300

    chore: service created

---
+++ src/services/Service.ts
@@ -0,0 +1,26 @@
+/* eslint-disable @typescript-eslint/no-wrapper-object-types */
+/* eslint-disable @typescript-eslint/no-unsafe-function-type */
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+});
+
+export const findItem = async (url: string, setData: Function) => {
+  const response = await api.get(url);
+  setData(response.data);
+};
+
+export const registerItem = async (url: string, data: Object, setData: Function) => {
+  const response = await api.post(url, data);
+  setData(response.data);
+};
+
+export const updateItem = async (url: string, data: Object, setData: Function) => {
+  const response = await api.get(url, data);
+  setData(response.data);
+};
+
+export const deleteItem = async(url: string) => {
+    await api.delete(url)
+}
